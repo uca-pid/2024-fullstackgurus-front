a53bfbf5f4c138dd03dd1e5c193b2fbd77525974
SebaDere <dere1997@gmail.com>
2024-11-07 15:12:19 -0300

    fix burger icon reesponsive

--- trainmate/src/pages/home/menu_responsive.tsx
+++ trainmate/src/pages/home/menu_responsive.tsx
@@ -63,19 +63,21 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ handleFilterOpen, handl
   return (
     <>
       {/* Hamburger icon for small screens */}
-      <IconButton
-        aria-label="menu"
-        onClick={handleDrawerOpen}
-        sx={{
-          display: { xs: 'block', sm: 'none' },
-          position: 'fixed',
-          top: 10,
-          right: 10,
-          zIndex: 9999,
-        }}
-      >
-        <MenuIcon sx={{ color: grey[50], fontSize: 40 }} />
-      </IconButton>
+      {!goalsDrawerOpen && (
+        <IconButton
+          aria-label="menu"
+          onClick={handleDrawerOpen}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            position: 'fixed',
+            top: 10,
+            right: 10,
+            zIndex: 9999,
+          }}
+        >
+          <MenuIcon sx={{ color: grey[50], fontSize: 40 }} />
+        </IconButton>
+      )}
 
       {/* Small screen drawer */}
       <Drawer
@@ -90,33 +92,33 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ handleFilterOpen, handl
           onClick={handleDrawerClose}
           onKeyDown={handleDrawerClose}
         >
-        <List sx={{mt: 5}}>
-          <ListItem onClick={handlePhysicalProgressOpen}>
-            <TrendingUpIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <ListItemText primary="Physical Progress" sx={{ marginLeft: 2 }} />
-          </ListItem>
-          <ListItem onClick={handleFilterOpen}>
-            <FilterAltIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <ListItemText primary="Filter By" sx={{ marginLeft: 2 }} />
-          </ListItem>
-          <ListItem onClick={handleClickOpen}>
-            <AddCircleOutlineIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <ListItemText primary="Add New" sx={{ marginLeft: 2 }} />
-          </ListItem>
-          <ListItem onClick={showGoalsDrawer}>
-            <OutlinedFlagIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <ListItemText primary="Goals" sx={{ marginLeft: 2 }} />
-          </ListItem>
-          <ListItem onClick={showDrawer}>
-            <CalendarMonthIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <ListItemText primary="See Agenda" sx={{ marginLeft: 2 }} />
-          </ListItem>
-        </List>
+          <List sx={{ mt: 5 }}>
+            <ListItem onClick={handlePhysicalProgressOpen}>
+              <TrendingUpIcon sx={{ color: grey[50], fontSize: 40 }} />
+              <ListItemText primary="Physical Progress" sx={{ marginLeft: 2 }} />
+            </ListItem>
+            <ListItem onClick={handleFilterOpen}>
+              <FilterAltIcon sx={{ color: grey[50], fontSize: 40 }} />
+              <ListItemText primary="Filter By" sx={{ marginLeft: 2 }} />
+            </ListItem>
+            <ListItem onClick={handleClickOpen}>
+              <AddCircleOutlineIcon sx={{ color: grey[50], fontSize: 40 }} />
+              <ListItemText primary="Add New" sx={{ marginLeft: 2 }} />
+            </ListItem>
+            <ListItem onClick={showGoalsDrawer}>
+              <OutlinedFlagIcon sx={{ color: grey[50], fontSize: 40 }} />
+              <ListItemText primary="Goals" sx={{ marginLeft: 2 }} />
+            </ListItem>
+            <ListItem onClick={showDrawer}>
+              <CalendarMonthIcon sx={{ color: grey[50], fontSize: 40 }} />
+              <ListItemText primary="See Agenda" sx={{ marginLeft: 2 }} />
+            </ListItem>
+          </List>
         </Box>
       </Drawer>
 
       {/* Normal buttons for large screens */}
-      <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 2}}>
+      <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 2 }}>
         <IconButton aria-label="filter" onClick={handlePhysicalProgressOpen}>
           <TrendingUpIcon sx={{ color: grey[50], fontSize: 40 }} />
           <p className='p-3 text-white'>Physical Progress</p>
@@ -130,9 +132,9 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ handleFilterOpen, handl
           <p className='p-3 text-white'>Add New</p>
         </IconButton>
         <IconButton onClick={showGoalsDrawer}>
-            <OutlinedFlagIcon sx={{ color: grey[50], fontSize: 40 }} />
-            <p className='p-3 text-white'>Goals</p>
-          </IconButton>
+          <OutlinedFlagIcon sx={{ color: grey[50], fontSize: 40 }} />
+          <p className='p-3 text-white'>Goals</p>
+        </IconButton>
         <IconButton aria-label="calendar" onClick={showDrawer}>
           <CalendarMonthIcon sx={{ color: grey[50], fontSize: 40 }} />
           <p className='p-3 text-white'>See Agenda</p>
@@ -140,10 +142,10 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ handleFilterOpen, handl
       </Box>
 
       <CalendarModal showDrawer={showDrawer} onClose={onClose} open={open} />
-      <GoalsModal showDrawer={showGoalsDrawer} onClose={onCloseGoalsDrawer} open={goalsDrawerOpen} openForm={openForm}/>
-      <ChallengeForm 
-        isOpen={showForm} 
-        onSave={() => {}} // Define saveChallenge if needed
+      <GoalsModal showDrawer={showGoalsDrawer} onClose={onCloseGoalsDrawer} open={goalsDrawerOpen} openForm={openForm} />
+      <ChallengeForm
+        isOpen={showForm}
+        onSave={() => { }} // Define saveChallenge if needed
         onCancel={closeForm} // Closes form and optionally reopens ChallengeModal
       />
     </>
